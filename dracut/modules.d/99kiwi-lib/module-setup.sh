26149f48f88edf3de927373649ba8b0fab6d73d3
Marcus Schäfer <ms@suse.de>
2017-09-22 15:50:13 +0200

    Prepare for luks, lvm and mdraid methods

--- dracut/modules.d/99kiwi-lib/module-setup.sh
+++ dracut/modules.d/99kiwi-lib/module-setup.sh
@@ -23,5 +23,13 @@ install() {
     inst_simple \
         "$moddir/kiwi-filesystem-lib.sh" "/lib/kiwi-filesystem-lib.sh"
     inst_simple \
+        "$moddir/kiwi-dialog-lib.sh" "/lib/kiwi-dialog-lib.sh"
+    inst_simple \
+        "$moddir/kiwi-mdraid-lib.sh" "/lib/kiwi-mdraid-lib.sh"
+    inst_simple \
+        "$moddir/kiwi-lvm-lib.sh" "/lib/kiwi-lvm-lib.sh"
+    inst_simple \
+        "$moddir/kiwi-luks-lib.sh" "/lib/kiwi-luks-lib.sh"
+    inst_simple \
         "/usr/share/fbiterm/fonts/b16.pcf.gz" "/lib/b16.pcf.gz"
 }
