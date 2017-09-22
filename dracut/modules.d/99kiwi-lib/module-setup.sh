2ecf0aeae9667574eeb6c73ee9cb1f181dc2117a
Marcus Schäfer <ms@suse.de>
2017-09-22 09:57:32 +0200

    Added collection of reworked dialog methods

--- dracut/modules.d/99kiwi-lib/module-setup.sh
+++ dracut/modules.d/99kiwi-lib/module-setup.sh
@@ -16,9 +16,12 @@ install() {
     inst_multiple \
         blkid parted dd mkdir grep fdasd cut tail head tr bc \
         btrfs xfs_growfs resize2fs \
-        e2fsck btrfsck xfs_repair
+        e2fsck btrfsck xfs_repair \
+        fbiterm
     inst_simple \
         "$moddir/kiwi-partitions-lib.sh" "/lib/kiwi-partitions-lib.sh"
     inst_simple \
         "$moddir/kiwi-filesystem-lib.sh" "/lib/kiwi-filesystem-lib.sh"
+    inst_simple \
+        "/usr/share/fbiterm/fonts/b16.pcf.gz" "/lib/b16.pcf.gz"
 }
